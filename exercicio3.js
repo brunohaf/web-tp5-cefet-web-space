5b92aba3916485b14230262227376d615922458c
Bruno Henrique Amaral Fonseca <brunof@take.net>
2021-11-28 22:26:13 -0300

    adds: article expand/retract events

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,15 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const expandClassName = 'expandido';
+const expandRetractButtonClassQuery = '.botao-expandir-retrair';
+let expandRetractButtonEls = document.querySelectorAll(expandRetractButtonClassQuery);
+
+const expandRetract = (evt) => {
+    let targetButton = evt.currentTarget;
+    targetButton.parentNode.classList.toggle(expandClassName)
+}
+
+expandRetractButtonEls.forEach((button) => {
+    button.addEventListener('click', expandRetract)
+})
